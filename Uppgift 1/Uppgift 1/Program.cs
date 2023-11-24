531409d2d336c8461e9aacfaf46e18e036279743
Hugo M <humy06001@utb.vaxjo.se>
2023-11-24 11:49:14 +0100

    programmeringsolympiaden

---
+++ Uppgift 1/Uppgift 1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Färger
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+
+            //-------------------------------------------
+
+            string word = "";
+
+            while (true)
+            {
+                Console.WriteLine("Vilket ord vill du upprepa?");
+                word = Console.ReadLine();
+
+                if (word.Length > 9)
+                {
+                    Console.WriteLine("Ordet får inte vara längre än 9 tecken.");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
+            Console.WriteLine("Hur många gånger ska ordet upprepas?");
+            int repeat = int.Parse(Console.ReadLine());
+            repeat = repeat > 9 ? 9 : repeat;
+
+            Console.WriteLine("Svar:");
+            for (int i = 0; i < repeat; i++)
+            {
+                Console.Write(word);
+            }
+        }
+    }
+}
+/*Reduplikation är när man upprepar ett ord flera gånger, ofta för att förstärka eller ändra
+betydelsen. I vissa språk som indonesiska är reduplikation väldigt vanligt, och där har
+det många olika grammatiska funktioner. Även i svenska används reduplikation ibland,
+till exempel är det vanligt att man säger "hej hej" istället för "hej".
+Rama är en flitig användare av reduplikation när han chattar med sina kompisar. Han
+skriver ibland samma ord upp till nio gånger, vilket tar lång tid. Därför brukar han
+istället skriva ordet en gång, åtföljt av en siffra som visar hur många gånger ordet ska
+upprepas.
+Skriv ett program som läser in ett ord som Rama tänker upprepa (som består av som
+mest 9 bokstäver) och hur många gånger det ska upprepas (som mest 9). Programmet
+ska sedan skriva ut ordet upprepat rätt antal gånger.
+Poängsättning: För testfall värda 1 poäng (utav 5 möjliga) gäller att antalet gånger
+ordet ska upprepas är 1.
+
+Körningsexempel 1
+Ordet ? hej
+Antal upprepningar ? 3
+Svar: hejhejhej
+
+Körningsexempel 2
+Ordet ? ha
+Antal upprepningar ? 4
+Svar: hahahaha*/
---
+++ Uppgift 2/Uppgift 2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Färger
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+
+            //-------------------------------------------
+
+            int N = 0;
+
+            Console.WriteLine("N =");
+            N = int.Parse(Console.ReadLine());
+            int antalTreigaTal = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if ((j*(j+1)*(j+2) == i))
+                    {
+                        antalTreigaTal++;
+                    }
+                    else if (j * (j + 1) * (j + 2) > i)
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("Svar: " + antalTreigaTal);
+        }
+    }
+}
+/*Matematikern Lenore Oljer har gjort så mycket matte att hon har tröttnat på primtal.
+Hon har nu börjat studera så kallade trevliga tripletter. Det är väldigt lätt att skapa en
+trevlig triplett. Börja med ett heltal a som är större än noll och skapa sedan tripletten
+(a, a + 1, a + 2). Alla tripletter som skapats på detta sätt är trevliga. Några exempel på
+trevliga tripletter är (4, 5, 6) och (15, 16, 17).
+För att lära sig mer om trevliga tripletter vill Lenore hitta hur många tal som kan
+skrivas som produkten av alla talen i en trevlig triplett. Hon kallar dessa tal “treiga
+tal”. Några exempel på treiga tal är 24 = 2 · 3 · 4 och 336 = 6 · 7 · 8. Lenore vill nu veta hur
+många treiga tal det finns som är mindre än talet N.
+Skriv ett program som läser in talet N och skriver ut hur många tal som är mindre än
+N och är treiga.
+Poängsättning:
+De fem testfallen som vardera ger 1 poäng har följande begränsningar:
+(1) N ≤ 25
+(2) N ≤ 80
+(3) N ≤ 1000
+(4) N ≤ 105
+(5) N ≤ 109
+
+Körningsexempel 1
+N ? 75
+Svar: 3
+Förklaring: De treiga talen mindre än 75 är 6, 24 och 60.
+Körningsexempel 2
+N ? 24
+Svar: 1
+Förklaring: Eftersom det treiga talet 24 inte är mindre än N räknar vi bara 6.
+Körningsexempel 3
+N ? 1234
+Svar: 9*/
